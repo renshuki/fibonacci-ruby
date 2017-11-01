04efcca9b242e11fef85de78404c99a40b3a89ae
renshuki <renshuki@mail.com>
2017-11-01 15:31:49 +0900

    Add titles on perf calculation

--- perf_calculation.sh
+++ perf_calculation.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+printf "\n#############\n"
+printf "# Recursive #\n"
+printf "#############\n"
 time ruby recursive.rb $1 $2
+
+printf "\n###############\n"
+printf "# Memoization #\n"
+printf "###############\n"
 time ruby memoization.rb $1 $2
+
+printf "\n#############\n"
+printf "# Iterative #\n"
+printf "#############\n"
 time ruby iterative.rb $1 $2
